0460460db0bce6c6a1d45f4998f34bf775f511fe
your Name <you@example.com>
2022-09-08 19:42:33 -0700

    using puts for display

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
-* main- print the string in the put function
-*
-* using puts instead of printf to display texts
-* this programme prints " programming is like building a multilingual puzzle
-* return 0
-*/
+ *
+ * * main - prints a string
+ *
+ * * Description: prints "Programming is like building a multilingual puzzle
+ *
+ * *Return: 0
+ *
+ * */
 int main(void)
 {
-	puts("\"programming is like a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
