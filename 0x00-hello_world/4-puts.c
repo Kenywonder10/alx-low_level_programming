56c3ffe77acbfd4065478811ffd1e8d41f526ed3
your Name <you@example.com>
2022-09-08 19:31:26 -0700

    using puts to display texts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
-
 /**
- * printing outputs on c using puts
- *
- * printing outputs without using printf in c 
- * my program ends with a value of zero
- */
+* main- print the string in the put function
+*
+* using puts instead of printf to display texts
+* this programme prints " programming is like building a multilingual puzzle
+* return 0
+*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\"programming is like a multilingual puzzle");
+	return(0);
 }
