05985e1dffbc8e8c951f29c32dfd0d9d83724751
your Name <you@example.com>
2022-09-08 17:05:34 -0700

    printing outputs in c using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * printing outputs on c using puts
+ *
+ * printing outputs without using printf in c 
+ * my program ends with a value of zero
+ */
+int main(){
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
