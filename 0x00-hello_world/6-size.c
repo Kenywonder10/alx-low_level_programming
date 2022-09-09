f7d1cabd13b2e6182c03a04616901c9995c5b07b
your Name <you@example.com>
2022-09-08 22:12:28 -0700

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,22 @@
-#include <stdio.h>
 /**
-*main -prints size
-*Description: prints size of the various variables in c in bytes
+*main - print the string in the put function
+*
+*Description: using the main function  
+*this program prints "Programming is like building a multilingual puzzle
 *Return: 0
 */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
 
-	Return(0);
-}
+	printf("size of a char: %ld byte(s)\n", sizeof(c));
+	printf("size of an int: %ld byte(s)\n", sizeof(i)); 
+	printf("size of a long: %ld byte(s)\n", sizeof(li)); 
+	printf("size of a long long int:%ld byte(s)\n", sizeof(lli)); 
+	printf("size of a float: %ld byte(s)\n", sizeof(f))
+	return (0);
+}	
