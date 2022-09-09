a00057c6ba78f0fc41e785b08a3d12ffba5a6936
your Name <you@example.com>
2022-09-08 21:21:44 -0700

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
-*main -print string
+*main - print the string in the put function
 *
-*Description: with proper grammar, but the outcome is a piece of art,
-*Return:0
+*Description: using the main function
+*this program prints "Programming is like building a multilingual puzzle
+*Return: 0
 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
