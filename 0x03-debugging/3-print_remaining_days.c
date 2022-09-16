b470e067dee0dc7edea308c4eb310bb3c873319d
your Name <you@example.com>
2022-09-15 17:35:59 -0700

    debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -25,11 +25,11 @@ else
 {
 if (month == 2 && day == 60)
 {
-printf("invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
 {
-printf("day of the year: %d\n", day);
+printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
 }
