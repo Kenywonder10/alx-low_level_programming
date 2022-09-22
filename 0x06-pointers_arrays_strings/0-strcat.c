4a7a6cc63f5cec0b1a94ee077e608a0df995fbe2
your Name <you@example.com>
2022-09-22 06:33:08 -0700

    more pointers arryas and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*_strcat - conatenates the string pointed to by @src to
+*_strcat - concatenates the string pointed to by @src to
 *the end of the string pointed to by @dest
 *@dest: String that will be appended
 *@src:  String to be concatenated upon
@@ -9,7 +9,7 @@
 *Return: returns pointer to @dest
 */
 
-char * _strcat(char *dest,char*src)
+char *_strcat(char *dest, char *src)
 {
 
 		int index = 0, dest_len = 0;
@@ -17,7 +17,7 @@ char * _strcat(char *dest,char*src)
 		while (dest[index++])
 			dest_len++;
 
-		for (index =0; src[index]; index++)
+		for (index = 0; src[index]; index++)
 			dest[dest_len++] = src[index];
 
 		return (dest);
