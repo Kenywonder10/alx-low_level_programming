5cdfa14c17ee42550e7548341c13c6d71e502b0e
your Name <you@example.com>
2022-09-26 18:53:45 -0700

    more pointers arrays and strings

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,11 +9,11 @@
 *Return: pointer to the copied memory block
 */
 
-char *memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
-	for (i < n; i++)
+	for (; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
