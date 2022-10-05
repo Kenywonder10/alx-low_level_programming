702e5c9b9c4c077752732e194a9d54466608083f
your Name <you@example.com>
2022-10-04 21:57:14 -0700

    malloc .c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -32,7 +32,6 @@ int i = 0;
 while (src[i] != '\0')
 {
 dest[i] = src[i];
-{
 i++;
 }
 dest[i] = '\0';
@@ -51,6 +50,15 @@ char *dst;
 unsigned int size;
 
 if (str == 0)
+{
+return (NULL);
+}
+
+size = _strlen(str) + 1;
+
+dst = (char *) malloc(size *sizeof(char));
+
+if (dst == 0)
 {
 return (NULL);
 }
