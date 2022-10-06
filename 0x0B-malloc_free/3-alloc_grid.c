13ed1273e9eb3419976855992188a65edd0b7148
your Name <you@example.com>
2022-10-05 20:25:04 -0700

    malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*alloc_grid - returns a pointer to a
+*@alloc_grid - returns a pointer to a
 *2 dimensionl array of integers.
 *@width: width of array
 *@height: height of array
