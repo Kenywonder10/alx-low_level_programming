b2108639d09af915277de82b3fee1feaa9566784
your Name <you@example.com>
2022-10-05 20:09:29 -0700

    argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
@@ -11,19 +12,29 @@
  */
 int main(int argc, char *argv[])
 {
-		int a = 0;
-		int b = 0;
-		int result = 0;
-
-		if (argc != 3)
-		{
-			printf("Error\n");
-			return (1);
-		}
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
-		result = a * b;
-
-		printf("%d\n", result);
-		return (0);
+int a, b;
+if (argc == 3)
+{
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+printf("%d\n", a *b);
+return (0);
 }
+printf("Error\n");
+return (1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
