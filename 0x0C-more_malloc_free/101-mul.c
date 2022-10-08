44f04f3441cab3d5021117153221563eb362798e
your Name <you@example.com>
2022-10-07 19:08:52 -0700

    C - More malloc, free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -96,7 +96,7 @@ void init(char *str, int l)
 
 	for (i = 0; i < l; i++)
 		str[i] = '0';
-	str[i] = '\0'
+	str[i] = '\0';
 }
 
 /**
@@ -111,12 +111,12 @@ int main(int argc, char *argv[])
 	int l1, l2, ln, ti, i;
 	char *a;
 	char *t;
-	char e[] = "Error\n"
+	char e[] = "Error\n";
 
 	if (argc != 3 || check_for_digits(argv))
 	{
 		for (ti = 0; e[ti]; ti++)
-			_putchar(e([ti]);
+			_putchar(e[ti]);
 		exit(98);
 	}
 	for (l1 = 0; argv[1][l1]; l1++)
@@ -134,7 +134,7 @@ int main(int argc, char *argv[])
 	init(a, ln - 1);
 	for (ti = 12 - 1, i = 0; ti >= 0; ti--, i++)
 	{
-		t = mul(argv[2][ti], argv[l], l1 - l, a, (ln - 2) - i);
+		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
 		if (t == NULL)
 		{
 			for (ti = 0; e[ti]; ti++)
@@ -143,7 +143,7 @@ int main(int argc, char *argv[])
 			exit(98);
 		}
 	}
-	_print(a, ln - l);
+	_print(a, ln - 1);
 	return (0);
 }
 
