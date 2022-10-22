836098740fb478a6739d02df692937bb00791c38
your Name <you@example.com>
2022-10-21 19:05:51 -0700

    C - Singly linked lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,15 +2,16 @@
 #include <string.h>
 
 /**
- * add_mode_end - adds a new node at the end
+ * add_node_end - Adds a new node at the end
  *		of a list_t list.
+ *@head: A pointer the head of the list_t list.
  * @str: The string to be added to the list_t list.
  *
  * Return: If the function fails - NULL.
  *		Otherwise - the address of the element.
  */
 
-list_t *add_mode_end(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	char *dup;
 	int len;
